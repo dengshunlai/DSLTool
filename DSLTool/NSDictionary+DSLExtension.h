72ad70a4ae2de80f1c51b97d2f509c997d7caf7e
dengshunlai <mu3305495@163.com>
2016-11-23 00:13:28 +0800

    code
    code

---
+++ DSLTool/NSDictionary+DSLExtension.h
@@ -0,0 +1,13 @@
+//
+//  NSDictionary+DSLExtension.h
+//  DSLTool
+//
+//  Created by 邓顺来 on 16/11/23.
+//  Copyright © 2016年 邓顺来. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSDictionary (DSLExtension)
+
+@end
---
+++ DSLTool/NSDictionary+DSLExtension.m
@@ -0,0 +1,13 @@
+//
+//  NSDictionary+DSLExtension.m
+//  DSLTool
+//
+//  Created by 邓顺来 on 16/11/23.
+//  Copyright © 2016年 邓顺来. All rights reserved.
+//
+
+#import "NSDictionary+DSLExtension.h"
+
+@implementation NSDictionary (DSLExtension)
+
+@end
