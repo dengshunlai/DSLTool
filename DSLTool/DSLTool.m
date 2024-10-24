aa03bc120ebce66a0922ef1d25bfc118bcb6fdef
邓顺来 <mu3305495@163.com>
2024-10-24 11:25:46 +0800

    code

--- DSLTool/DSLTool.m
+++ DSLTool/DSLTool.m
@@ -137,31 +137,31 @@
 }
 
 + (UIViewController *)topViewController {
-    UIViewController *topVC = nil;
-    AppDelegate *appDelegate = (AppDelegate *)[UIApplication sharedApplication].delegate;
-    UIWindow *mainWindow = appDelegate.window;
-    
-    UIViewController *lastPresentedVC = mainWindow.rootViewController;
-    while (lastPresentedVC.presentedViewController) {
-        lastPresentedVC = lastPresentedVC.presentedViewController;
-    }
-    
-    if ([lastPresentedVC isKindOfClass:[UINavigationController class]]) {
-        UINavigationController *nc = (UINavigationController *)lastPresentedVC;
-        topVC = nc.visibleViewController;
-    } else if ([lastPresentedVC isKindOfClass:[UITabBarController class]]) {
-        UITabBarController *tc = (UITabBarController *)lastPresentedVC;
-        UIViewController *vc = tc.viewControllers[tc.selectedIndex];
-        if ([vc isKindOfClass:[UINavigationController class]]) {
-            UINavigationController *nc = (UINavigationController *)vc;
-            topVC = nc.visibleViewController;
-        } else {
-            topVC = vc;
-        }
-    } else {
-        topVC = lastPresentedVC;
-    }
+    UIViewController *vc = [UIApplication sharedApplication].keyWindow.rootViewController;
+    UIViewController *topVC = [self findNextViewControllerFrom:vc];
     return topVC;
+}
+
++ (UIViewController *)findNextViewControllerFrom:(UIViewController *)vc {
+    //递归
+    UIViewController *currentShowingVC;
+    if ([vc presentedViewController]) {
+        // 当前视图是被presented出来的
+        UIViewController *nextRootVC = [vc presentedViewController];
+        currentShowingVC = [self findNextViewControllerFrom:nextRootVC];
+    } else if ([vc isKindOfClass:[UITabBarController class]]) {
+        // 根视图为UITabBarController
+        UIViewController *nextRootVC = [(UITabBarController *)vc selectedViewController];
+        currentShowingVC = [self findNextViewControllerFrom:nextRootVC];
+    } else if ([vc isKindOfClass:[UINavigationController class]]){
+        // 根视图为UINavigationController
+        UIViewController *nextRootVC = [(UINavigationController *)vc visibleViewController];
+        currentShowingVC = [self findNextViewControllerFrom:nextRootVC];
+    } else {
+        // 根视图为非导航类
+        currentShowingVC = vc;
+    }
+    return currentShowingVC;
 }
 
 @end
